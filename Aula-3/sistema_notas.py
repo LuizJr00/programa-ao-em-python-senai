0713aecd1cd19d543aa730c0613bff8f52c73950
LuizJr00 <luiz.edmundo.jr15@gmail.com>
2026-07-17 21:45:02 -0300

    Add files via upload

---
+++ Aula-3/sistema_notas.py
@@ -0,0 +1,33 @@
+
+
+nota_1 = 3
+nota_2 = 5
+nota_3 = 6
+
+media = (nota_1 + nota_2 + nota_3)/3
+print(media)
+
+aprovado = media >= 7
+recuperacao = media >=5 and media <7
+reprovado = media < 5
+
+print('situacao do aluno:')
+print('Aprovado:')
+print(aprovado)
+print('Recuperacao:')
+print(recuperacao)
+print('Reprovado:')
+print(reprovado)
+
+n1 = 5
+n2 = 5
+soma = n1 + n2
+subtracao = n1 - n2
+divisao = n1 / n2
+multiplicacao = n1 * n2
+print(soma)
+print(subtracao)
+print(divisao)
+print(multiplicacao)
+
+print('calculadora python')
